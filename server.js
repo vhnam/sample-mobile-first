a9c79de57b1a756da2a01cccb11301445ca95ff2
Nam Vo <vhnam2504@gmail.com>
2020-06-17 20:41:53 +0700

    chore: setup HTTP2 Server-Push

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 const compression = require('compression');
-const spdy = require('spdy');
+const http2 = require('spdy');
 
 const app = express();
 
@@ -15,16 +15,53 @@ const options = {
   cert: fs.readFileSync(__dirname + '/certs/server.crt'),
 };
 
+const manifest = JSON.parse(fs.readFileSync('./manifest.json'));
+
+const pushAsset = (res, path, type, file) => {
+  res.push(
+    path,
+    {
+      status: 200,
+      method: 'GET',
+      request: {
+        accept: '*/*',
+      },
+      response: {
+        'content-type': type,
+      },
+    },
+    (error, stream) => {
+      if (error) {
+        return;
+      }
+      stream.end(file);
+    },
+  );
+};
+
+const pushGeneralAssets = (res) => {
+  let content;
+  Object.keys(manifest).forEach((group) => {
+    manifest[group].forEach((file) => {
+      content = fs.readFileSync(__dirname + file.path);
+      pushAsset(res, file.path, file.type, content);
+    });
+  });
+};
+
 app.disable('x-powered-by');
 
 app.use(compression());
 app.use('/assets', express.static('assets'));
 
 app.get('/', (req, res) => {
+  if ('production' === process.env.NODE_ENV) {
+    pushGeneralAssets(res);
+  }
   res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-spdy.createServer(options, app).listen(process.env.NODE_PORT, (error) => {
+http2.createServer(options, app).listen(process.env.NODE_PORT, (error) => {
   if (error) {
     console.error(error);
     return process.exit(1);
